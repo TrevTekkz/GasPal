4041ef07feb20e76a92f61a2dcde0d703cb46cd3
TrevTekkz <100790111+TrevTekkz@users.noreply.github.com>
2022-10-25 18:26:16 -0400

    Add project files.

---
+++ GasPal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GasPal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
